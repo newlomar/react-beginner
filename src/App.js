aa87d47a26c9f3d7e3c6da0ff5619cff23c562e8
Newton Lomar <newtonlomar16@gmail.com>
2021-06-24 05:56:03 -0300

    more react classes

--- src/App.js
+++ src/App.js
@@ -17,14 +17,22 @@ import './appStyles.css'
 import styles from './appStyles.module.css'
 import Form from './components/Form'
 import LifecycleA from './components/LifecycleA';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
+import PureComp from './components/PureComp';
+import ParentComp from './components/ParentComp';
 
 function App() {
   return (
 
     
     <div className="App">
-    <LifecycleA />
-    {/* 
+      
+    {/*
+    <ParentComp />
+    <Table />
+    <FragmentDemo />
+    <LifecycleA /> 
     <Form />
     <h1 className='error'>Error</h1>
     <h1 className={styles.success}>Success</h1>
