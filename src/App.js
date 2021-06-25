9f0f07830e23b0da15e4b37bc70d148fc5092d3b
Newton Lomar <newtonlomar16@gmail.com>
2021-06-25 07:51:06 -0300

    more react classes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 // import { Greet } from './components/Greet';
 // import Welcome from './components/Welcome';
@@ -29,74 +30,81 @@ import './App.css';
 // import ErrorBoundary from './components/ErrorBoundary';
 // import ClickCounter from './components/ClickCounter';
 // import HoverCounter from './components/HoverCounter';
-import ClickCounterTwo from './components/ClickCounterTwo';
-import HoverCounterTwo from './components/HoverCounterTwo';
+// import ClickCounterTwo from './components/ClickCounterTwo';
+// import HoverCounterTwo from './components/HoverCounterTwo';
 // import User from './components/User';
-import PropCounter from './components/PropCounter';
+// import PropCounter from './components/PropCounter';
+import ComponentC from './components/ComponentC'
+import { UserProvider } from './components/userContext';
 
-function App() {
-  return (
-
-    <div className="App">       
-      <PropCounter> 
-        {(count, incrementCount) => (
-          <ClickCounterTwo count={count} incrementCount={incrementCount} />
-        )}
-      </PropCounter>
-      <PropCounter> 
-        {(count, incrementCount) => (
-          <HoverCounterTwo count={count} incrementCount={incrementCount} />
-        )}
-      </PropCounter>
-    {/*
-    <ClickCounterTwo />
-    <HoverCounterTwo />
-    <User render={(isLoggedIn) => isLoggedIn ? 'Newton' : 'Guest'} />
-    <ClickCounter name='Newton' />
-    <HoverCounter />
-    <ErrorBoundary>
-        <Hero heroName="Batman" />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Superman" />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Joker" />
-      </ErrorBoundary>
-    <PortalDemo />
-    <FRParentInput />
-    <FocusInput />
-    <RefsDemo />
-    <ParentComp />
-    <Table />
-    <FragmentDemo />
-    <LifecycleA /> 
-    <Form />
-    <h1 className='error'>Error</h1>
-    <h1 className={styles.success}>Success</h1>
-    <Inline />
-    <Stylesheet primary={true}/>
-      <NameList />
-      <UserGreeting />
-      <ParentComponent />
-      <EventBind />
-      <FunctionClick />
-      <ClassClick />
-      <Counter />
-      <Message />
-      // <Greet name="Beatriz" parentName="Girlfriend" >
-      //   <p>This is children props</p>
-      // </Greet>
-      // <Greet name="Luciene" parentName="Mother">
-      //   <button>Botao</button>
-      // </Greet>
-      // <Greet name="João" parentName="Brother" />
-      <Welcome name="Beatriz" parentName="Girlfriend" />
-      <Welcome name="Luciene" parentName="Mother" />
-      <Welcome name="João" parentName="Brother"/>
-      <Hello /> */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <UserProvider value="Newton">
+          <ComponentC />
+        </UserProvider>
+      </div>
+    )
+  }
 }
+      
+    // {/*
+    // <PropCounter> 
+    //     {(count, incrementCount) => (
+    //       <ClickCounterTwo count={count} incrementCount={incrementCount} />
+    //     )}
+    //   </PropCounter>
+    //   <PropCounter> 
+    //     {(count, incrementCount) => (
+    //       <HoverCounterTwo count={count} incrementCount={incrementCount} />
+    //     )}
+    // </PropCounter>
+    // <ClickCounterTwo />
+    // <HoverCounterTwo />
+    // <User render={(isLoggedIn) => isLoggedIn ? 'Newton' : 'Guest'} />
+    // <ClickCounter name='Newton' />
+    // <HoverCounter />
+    // <ErrorBoundary>
+    //     <Hero heroName="Batman" />
+    //   </ErrorBoundary>
+    //   <ErrorBoundary>
+    //     <Hero heroName="Superman" />
+    //   </ErrorBoundary>
+    //   <ErrorBoundary>
+    //     <Hero heroName="Joker" />
+    //   </ErrorBoundary>
+    // <PortalDemo />
+    // <FRParentInput />
+    // <FocusInput />
+    // <RefsDemo />
+    // <ParentComp />
+    // <Table />
+    // <FragmentDemo />
+    // <LifecycleA /> 
+    // <Form />
+    // <h1 className='error'>Error</h1>
+    // <h1 className={styles.success}>Success</h1>
+    // <Inline />
+    // <Stylesheet primary={true}/>
+    //   <NameList />
+    //   <UserGreeting />
+    //   <ParentComponent />
+    //   <EventBind />
+    //   <FunctionClick />
+    //   <ClassClick />
+    //   <Counter />
+    //   <Message />
+    //   // <Greet name="Beatriz" parentName="Girlfriend" >
+    //   //   <p>This is children props</p>
+    //   // </Greet>
+    //   // <Greet name="Luciene" parentName="Mother">
+    //   //   <button>Botao</button>
+    //   // </Greet>
+    //   // <Greet name="João" parentName="Brother" />
+    //   <Welcome name="Beatriz" parentName="Girlfriend" />
+    //   <Welcome name="Luciene" parentName="Mother" />
+    //   <Welcome name="João" parentName="Brother"/>
+    //   <Hello /> */}
 
 export default App;
